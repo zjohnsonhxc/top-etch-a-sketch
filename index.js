b6cea10f2aa62aeed401a8cb19e5d2fc19888342
zjohnsonhxc <hexcassette@gmail.com>
2023-01-29 20:18:40 -0700

    Make minor CSS changes
    Change background color, centering, and some Flexbox elements

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const container = document.querySelector('.container');
+// these are the three buttons that select the size of the grid
 const tenByTen = document.querySelector('.ten');
 const twentyByTwenty = document.querySelector('.twenty');
 const thirtyByThirty = document.querySelector('.thirty');
 
+// function that creates the Etch-a-Sketch grid, tied to all grid-size buttons via event listeners
 function createEtchASketch(totalSquares, squareHeight, rows) {
-    console.log(totalSquares);
+    // clear existing grid
     container.innerHTML = '';
+    // create grid based on totalSquares, squareHeight and rows of button picked
     for (let i = 0; i < totalSquares; i++) 
     {
     const div = document.createElement('div');
+    // create new 'opacity' style for each div;
     let opacity = 0.0;
     div.style.cssText = `border: 1px solid black; height: ${squareHeight}px; width: ${squareHeight}px; background-color: white`;
     container.style.cssText = `grid-template-columns: repeat(${rows}, ${squareHeight}px [col-start])`;
+    // changes color for each separate div 
     div.addEventListener('mouseover', function() {
         opacity += 0.33;
         div.style.backgroundColor = `rgba(0, 255, 0, ${opacity})`;
